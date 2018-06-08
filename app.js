d162c2060a11cbb5e792b8de4fec79d4c4c1d338
Areanto <ferdiareanto@gmail.com>
2018-06-08 21:37:49 +0700

    Create function constructors for inc and exp items

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 // BUDGET CONTROLLLER
 var budgetController = (function () {
 
-    // some code
+    // each new item needs description and a value + distinguish by #id income vs. expense
+    
+    
+    // create a function constructor for income and expense types
 
+    var Expenses = function(id, deescription,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Incomes = function(id, deescription,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    // Data store
+    var data = {
+        allItems: {
+            exp:[],
+            inc:[]
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 })();
 
 
@@ -38,54 +64,54 @@ var UIController = (function () {
 
 
 
-
-// GLOBAL APP CONTROLLER 
-var controller = (function (budgetCtrl, UICtrl) {
-
+ 
+ // GLOBAL APP CONTROLLER 
+ var controller = (function(budgetCtrl, UICtrl) {
+     
     // private function that sets up the event listeners
-    var setupEventListeners = function () {
-
+    var setupEventListeners = function() {
+       
         var DOM = UICtrl.getDOMstrings();
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-        document.addEventListener('keypress', function (event) {
-
+    
+        document.addEventListener('keypress', function(event){
+        
             // use .which to add support for older browsers
-            if (event.keyCode === 13 || event.which === 13) {
-                ctrlAddItem();
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem(); 
             }
-
+        
         });
     };
-
-
-
+    
+    
+      
     // private function that gets called when we want to add a new item
-    var ctrlAddItem = function () {
-
+    var ctrlAddItem = function(){
+        
         // 1. Get the field input data when enter key or button is clicked
         var input = UICtrl.getInput();
-
+        
         // 2. Add the item to the budget controller
-
-
+        
+        
         // 3. Add the new item to the UI
-
-
+        
+        
         // 4. Calculate the budget
-
-
+        
+        
         // 5. Display the budget on the UI
-
+        
     };
 
-
-    // create a public initialization function
-    // return in an object to make public
-
+         
+     // create a public initialization function
+     // return in an object to make public
+     
 
     return {
-        init: function () {
+        init: function() {
             console.log("application has started");
             setupEventListeners();
         }
