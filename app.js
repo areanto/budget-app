8bf78ffc25e20aa879616fe6c6118ffbda71c57c
Areanto <ferdiareanto@gmail.com>
2018-06-09 16:38:28 +0700

    Add new item to UI controller & UI

--- app.js
+++ app.js
@@ -81,7 +81,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     };
 
     // return an object that contains a method to get input values
@@ -92,6 +94,34 @@ var UIController = (function () {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+        addListItem: function (obj, type) {
+            
+            // declare variables
+            var html, newHtml, element;
+            
+            // create HTML string with placeholder text
+            if (type === 'inc') {
+                
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                
+            } else if (type === 'exp') {
+                
+                element = DOMstrings.expenseContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                
+            }
+            
+            // replace placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);  
+            
+            
         },
         // pass DOMstrings object to the global app controller
         getDOMstrings: function () {
@@ -134,7 +164,8 @@ var UIController = (function () {
         // 2. Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         
-        // 3. Add the new item to the UI
+        // 3. Add the new item to the 
+        UICtrl.addListItem(newItem, input.type);
         
         
         // 4. Calculate the budget
